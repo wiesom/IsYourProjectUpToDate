9b4b9e79f0542c2222970cb88f51d84f0cf265cf
Karl Lindmark <karl.lindmark@ninetwozero.com>
2014-09-12 19:12:39 +0200

    Production proof the server

--- UpToDate/settings.py
+++ UpToDate/settings.py
@@ -13,19 +13,11 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__)+'/..')
 
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "DEBUG")
+DEBUG = SECRET_KEY == "DEBUG"
+TEMPLATE_DEBUG = DEBUG
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't&tn1o8&6hcw)n)(x85tnamnqw=e=av^$h_jcqt8=&hi2pj_(4'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-TEMPLATE_DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['iyputd.ninetwozero.com']
 
 
 # Application definition
@@ -68,15 +60,10 @@ DATABASES = {
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
@@ -115,9 +102,7 @@ LOGGING = {
 }
 
 CONFIG = {
-    # 'mode': 'wsgi',
     'working_dir': '/var/www/public/iyputd.com/public/UpToDate',
-    # 'python': '/usr/bin/python',
     'args': (
         '--bind=127.0.0.1:8000',
         '--workers=5',
